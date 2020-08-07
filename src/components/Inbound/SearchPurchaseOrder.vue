79ce10e6ca2a1c6c6c76505e04b936f44b05da1f
Tai Beo <tai.nguyentrung1996@gmail.com>
2020-08-07 14:09:42 +0700

    Fix image size PO detail and fix loading PO list and Fix UI Detail and Loading Search

--- src/components/Inbound/SearchPurchaseOrder.vue
+++ src/components/Inbound/SearchPurchaseOrder.vue
@@ -2,7 +2,8 @@
   <div>
     <form>
       <div class="input-group mb-3">
-        <input type="search" class="form-control inp-search" placeholder="Nhập PO cần tìm (Vd: PO1111)" v-model="searchInput" @input="onInputSearchPO">
+        <input type="search" class="input-search" placeholder="Nhập PO cần tìm (Vd: PO1111)" v-model="searchInput" @input="onInputSearchPO" />
+        <i class="fas fa-search text-muted position-icon"></i>
       </div>
     </form>
   </div>
@@ -25,7 +26,28 @@ export default {
 </script>
 
 <style lang="scss">
-  .inp-search {
-    border-radius: 35px !important;
+.position-icon {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  margin-left: 0.8em;
+}
+.input-search {
+  border: 1px solid  #919aa3;
+  border-radius: 35px !important;
+  padding: 0.5em 1em 0.5em 2.5em;
+  width: 30%;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 99;
   }
+
+  &:focus {
+    outline: none;
+  }
+}
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -27,20 +27,28 @@
         <div class="detail-info__item">Delivery</div>
         <div class="detail-info__item">{{ purchaseOrderDetail.name }}</div>
       </div>
-      <div class="my-3 px-3 position-relative">
-        <i class="fas fa-search text-muted position-center"></i>
-        <input type="search" class="w-25 input-search pl-5" placeholder="Some Input Text" name id />
+
+      <div class="container-fluid">
+        <div class="form-group px-5 form-inline">
+          <div class="my-3 px-3 position-relative">
+            <i class="fas fa-search text-muted position-icon"></i>
+            <input type="search" class="input-search" placeholder="Nhập tên thuốc cần tìm..." v-model="searchInput" @keyup="getPODetailById" />
+          </div>
+          <button type="button" class="btn btn-primary ml-auto">Hoàn tất</button>
+        </div>
       </div>
+
       <div class="container-fluid bg-light">
         <div class="px-5">
           <div class="list" v-for="product in purchaseOrderDetail.lines" :key="product.id">
+            {{ product }}
             <div class="list-info py-2">
               <div class="list-info__id p-5">{{ product.id }}</div>
               <div class="list-info__image justify-content-center d-flex px-2">
                 <img class="img-product"
                   :src="product.image_url ? product.image_url : 'https://assets.thuocsi.vn/assets/defaults/missing-e9cfa4812c342b9780b61700d2ade43591b3c5992f4dffedaa542c07d854b602.png'"
                   :alt="product.name"
-                  width="200" height="200"
+                  width="150" height="150"
                 />
               </div>
               <div class="list-info__content px-3">
@@ -174,10 +182,10 @@ import axios from "axios";
 
 export default {
   name: "detail",
-  components: {},
   data() {
     return {
       date: "",
+      searchInput: "",
       fields: [
         { key: "done_qty", label: "Số lượng nhập" },
         { key: "lot_number", label: "Lot Number" },
@@ -209,22 +217,31 @@ export default {
     };
   },
   created() {
-    const id = this.$route.params.id;
-    axios.get(`https://cors-anywhere.herokuapp.com/https://erp.stg.thuocsi.vn/api/v1/receipts/${id}`,
-                { headers:
-                  {
-                    "api-key": "1fce20616fd2500d63b980b6f37ea8c288378604f47e282ff8644fc5529ebb75"
-                  }
-                })
-    .then(res => {
-      this.purchaseOrderDetail = res.data
-      this.isLoading = false;
-    })
-    .catch(err => {
-      console.log(`Error get List PO ${err}`)
-    });
+    this.getPODetailById();
   },
   methods: {
+    getPODetailById() {
+      return axios.get(`https://cors-anywhere.herokuapp.com/https://erp.stg.thuocsi.vn/api/v1/receipts/${this.$route.params.id}`,
+                  { headers:
+                    {
+                      "api-key": "1fce20616fd2500d63b980b6f37ea8c288378604f47e282ff8644fc5529ebb75",
+                      "Access-Control-Allow-Origin": "*"
+                    }
+                  })
+      .then(res => {
+        if (this.searchInput.length === 0) {
+          this.purchaseOrderDetail = res.data;
+          this.isLoading = false;
+        } else {
+          console.log(this.purchaseOrderDetail)
+          this.purchaseOrderDetail = res.data.lines.filter(product => {
+            product.name.toLowerCase().includes(this.searchInput.toLowerCase())
+          })
+        }
+      }).catch(err => {
+        console.log(`Error get List PO ${err}`)
+      });
+    },
     handleAdd() {
       if (this.dataSource.length < 3) {
         const newData = {
@@ -244,18 +261,20 @@ export default {
         if (value) {
           const dataSource = [...this.dataSource];
           this.dataSource = dataSource.filter((item) => item.key !== key);
+          this.isAddLot = false;
         }
       });
     },
     printQRCode() {
+      // TODO: Need Implement action print QR Code
       this.isPrintQRCode = true;
-    }
-  },
+    },
+  }
 };
 </script>
 
 <style lang="scss">
-.position-center {
+.position-icon {
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
@@ -290,9 +309,12 @@ export default {
 }
 
 .input-search {
-  border: 1px solid #919aa3;
-  padding: 0.5em 1em;
-  border-radius: 1em;
+  border-radius: 35px;
+  border: 1px solid  #919aa3;
+  border-radius: 35px !important;
+  padding: 0.5em 1em 0.5em 2.5em;
+  width: 30%;
+  min-width: 100%;
 
   &::before {
     content: "aa";
